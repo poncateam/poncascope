b168adca78d294061344f3b83d8b301d875f2338
fauberva <florian.auberval@irit.fr>
2026-04-15 10:09:46 +0200

    Rollback to master on some functions
    Signed-off-by: fauberva <florian.auberval@irit.fr>

--- src/main.cpp
+++ src/main.cpp
@@ -144,12 +144,6 @@ using FitASODiff = Ponca::BasketDiff<
         Ponca::OrientedSphereDer, Ponca::MlsSphereFitDer,
         Ponca::CurvatureEstimatorDer, Ponca::NormalDerivativeWeingartenEstimator>;
 
-using FitASODiff = Ponca::BasketDiff<
-        FitASO,
-        Ponca::DiffType::FitSpaceDer,
-        Ponca::OrientedSphereDer, Ponca::MlsSphereFitDer,
-        Ponca::CurvatureEstimatorDer, Ponca::NormalDerivativeWeingartenEstimator>;
-
 using FitCNCUniform = Ponca::CNC<PPAdapter, Ponca::TriangleGenerationMethod::UniformGeneration>;
 using FitCNCIndep   = Ponca::CNC<PPAdapter, Ponca::TriangleGenerationMethod::IndependentGeneration>;
 using FitCNCHex     = Ponca::CNC<PPAdapter, Ponca::TriangleGenerationMethod::HexagramGeneration>;
@@ -170,7 +164,7 @@ void processPointCloudMLS(const typename FitT::Scalar t, const Functor& f){
             fit.computeWithIdsMLS(rangeNeighbors, tree.points(), mlsIter);
         });
 
-        const VectorType projectedPos = static_cast<const FitT&>(fit).getNeighborFilter().evalPos();
+        const auto& projectedPos = fit.getNeighborFilter().evalPos();
 
         if (fit.isStable()) {
             f(i, fit, projectedPos);
@@ -292,22 +286,17 @@ inline void mlsDryRun() {
 template<typename FitT, bool isSigned = true>
 Scalar evalScalarField_impl(const VectorType& input_pos)
 {
-    VectorType current_pos = input_pos;
-    Scalar current_value = std::numeric_limits<Scalar>::max();
-    for(int mm = 0; mm < mlsIter; ++mm)
-    {
-            FitT fit;
-            fit.setNeighborFilter({current_pos, NSize}); // weighting function using current pos (not input pos)
-            auto res = fit.computeWithIds(tree.rangeNeighbors(current_pos, NSize), tree.points());
-            if(res == Ponca::STABLE) {
-            current_pos = fit.project(input_pos); // always project input pos
-            current_value = isSigned ? fit.potential(input_pos) : std::abs(fit.potential(input_pos));
-            // current_gradient = fit.primitiveGradient(input_pos);
-        } else {
-            // not enough neighbors (if far from the point cloud)
-            return .0;//std::numeric_limits<Scalar>::max();
-        }
+    FitT fit;
+    fit.setNeighborFilter({input_pos, NSize}); // weighting function using current pos (not input pos)
+    auto res = fit.computeWithIdsMLS(tree.rangeNeighbors(input_pos, NSize), tree.points(), mlsIter);
+
+    if(!fit.isStable()) {
+        // not enough neighbors (if far from the point cloud)
+        return Scalar(0); //std::numeric_limits<Scalar>::max();
     }
+
+    const Scalar current_value = isSigned ? fit.potential(input_pos) : std::abs(fit.potential(input_pos));
+    // current_gradient = fit.primitiveGradient(input_pos);
     return current_value;
 }
 
